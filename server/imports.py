9ea3e580a77ff64b9dfda000c01e321934606271
Tonya E <luis1944alex@gmail.com>
2023-04-25 13:43:51 -0400

    Added imports, dependencies, and requirements files. And README.md

---
+++ server/imports.py
@@ -0,0 +1,11 @@
+# Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+# Tensor Flow
+import tensorflow as tf
+import tensorflow_hub as hub
+
+# Numpy and Pandas
+import numpy as np
+import pandas as pd
