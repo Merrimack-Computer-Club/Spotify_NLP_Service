14857a268dd0f0c2bcede9a162f685b9cf9b8b0c
Derek Costello <costellod@merrimack.edu>
2023-05-02 14:40:08 -0400

    Restructuring architecture to be more straightforward

--- server/model.py
+++ server/model.py
@@ -1,29 +1,28 @@
 from imports import *
 from bert_utils import create_tokenizer_from_hub_module, tokenize_sentences
-from transformers import BertTokenizer, BertModel
+from transformers import BertTokenizer, BertModel, AdamW, get_linear_schedule_with_warmup
+from sklearn.model_selection import train_test_split
 
-# Class built for our BERT Classifier (called Model).
-# Resource(s): https://skimai.com/fine-tuning-bert-for-sentiment-analysis/
-class Model(nn.Module):
+# Class built for our BERT Classifier.
+# Source: https://skimai.com/fine-tuning-bert-for-sentiment-analysis/
+class BertClassifier(nn.Module):
 
-    # Constructor, creates the model
+    # Constructor, creates the classifier
     def __init__(self, freeze_bert = False):
         """
-        @param  model: BertModel object
-        @param  tokenizer: BertTokenizer object
+        @param  bert_model: BertModel object
         @param  freeze_bert (bool): Set `False` to fine-tune the BERT model (according to the resource)
         @param  classifier: torch.nn.Module classifier
             #    Note: torch.nn.Module is used to help train / build        #
             #       neural networks, so we will build on BERT with this     #
         """
-        super(Model, self).__init__()
+        super(BertClassifier, self).__init__()
         # Specify output size of BERT model (768), hidden size of our classifier (50), and number of labels (28)
         D_in, H, D_out = 768, 50, 28
 
 
         # Instantiate the pre-trained BERT tokenizer and model
-        self.model = BertModel.from_pretrained('bert-base-uncased')
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.bert_model = BertModel.from_pretrained('bert-base-uncased')
 
         # Instantiate one-layer feed-forward classifer
         self.classifier = nn.Sequential(
@@ -33,7 +32,7 @@ class Model(nn.Module):
         )
         # Freeze the BERT model
         if freeze_bert:
-            for param in self.bert.parameters():
+            for param in self.bert_model.parameters():
                 param.requires_grad = False
 
     # Define a feed-forward function to compute the logits
@@ -46,7 +45,7 @@ class Model(nn.Module):
         @return   logits (torch.Tensor): an output tensor
         """
         # Feed input to BERT
-        outputs = self.bert(input_ids=ids, attention_mask=mask)
+        outputs = self.bert_model(input_ids=ids, attention_mask=mask)
         
         # Extract the last hidden state of the token `[CLS]` for classification task
         last_hidden_state_cls = outputs[0][:, 0, :]
@@ -55,6 +54,52 @@ class Model(nn.Module):
         logits = self.classifier(last_hidden_state_cls)
 
         return logits
+    
+class Model():
+    # Constructor, creates the classifier
+    def __init__(self, bert_classifier):
+        """
+        @param  bert_classifier: BertClassifier object
+        @param  tokenizer: BertTokenizer object
+        """
+        
+        # Instantiate the classifier
+        self.bert_classifier = bert_classifier
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    # Initializing the model, optimizer, and learning rate scheduler for training
+    # Source: https://skimai.com/fine-tuning-bert-for-sentiment-analysis/
+    def initialize_model(epochs=3):
+        # Initialize the classifier model, the optimizer, and the learning rate scheduler
+        classifier = BertClassifier(freeze_bert=False) #Instantiate the model
+        # Try to use GPU (cuda). Otherwise, we will have to use CPU
+        device = None
+        if torch.cuda.is_available():       
+            device = torch.device("cuda")
+            print(f'There are {torch.cuda.device_count()} GPU(s) available.')
+            print('Device name:', torch.cuda.get_device_name(0))
+        else:
+            print('No GPU available, using the CPU instead.')
+            device = torch.device("cpu")
+
+        # Assign the model to hardware
+        classifier.to(device)
+
+        # Create an AdamW optimizer
+        optimizer = AdamW(classifier.parameters(),
+                        lr=5e-5,   # Best learning rate (lr) described. Also default
+                        eps=1e-8)  # Default epsilon value
+        
+        # Total number of training steps for the lr scheduler
+        total_steps = len(train_data_loader) * epochs
+
+        # Set up learning rate scheduler for our model
+        scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                    num_warmup_steps=0, #Default
+                                                    num_training_steps=total_steps)
+        
+        return classifier, optimizer, scheduler
+
     
     # Define a function to tokenize the input and prepare it for fine-tuning
     def tokenize_inputs(self, data):
@@ -93,10 +138,6 @@ class Model(nn.Module):
         # Return the tokenized inputs and labels
         return input_ids, attention_masks, labels
 
-
-    
-model = Model()
-
 def to_data_loader(input_ids, attention_masks, labels):
     # Place data in a 
         # Place data in a PyTorch DataLoader (faster training / less resources)
@@ -105,14 +146,15 @@ def to_data_loader(input_ids, attention_masks, labels):
     data_loader = DataLoader(data, sampler=data_sampler, batch_size=32)
     return data_loader
 
-def fine_tune(model, data_loader):
-    # Freeze the BERT layers to start (that way we can start fine-tuning)
-    for param in model.parameters():
-        param.requires_grad = False
+
+###                 Main Code                   ###
 
 
 # Load the CSV data (expected to run from folder: "./Artificial Intelligence/Spotify_NLP_Service")
 df = pd.read_csv('./server/data/testemotions_1.csv')
+
+df_train = df.sample(frac=0.8, random_state=200)
+df_test = df.drop(df_train.index)
 
 # Define the emotion labels
 emotion_labels = ["admiration", "amusement", "anger", "annoyance", "approval", "caring", 
@@ -123,12 +165,16 @@ emotion_labels = ["admiration", "amusement", "anger", "annoyance", "approval", "
 
 # Step 1: Tokenize inputs so BERT can read it. 
     # Tokenize the inputs
-input_ids, attention_masks, labels = model.tokenize_inputs(df)
+train_input_ids, train_attention_masks, train_labels = model.tokenize_inputs(df_train)
+test_input_ids, test_attention_masks, test_labels = model.tokenize_inputs(df_test)
+
 
 # Step 2: Fine-tune BERT.
 # Resource(s): https://www.youtube.com/watch?v=mw7ay38--ak
-data_loader = to_data_loader(input_ids, attention_masks, labels)
-fine_tune(model, data_loader)
+train_data_loader = to_data_loader(train_input_ids, train_attention_masks, train_labels)
+
+
+###                 End Main Code                   ###
 
 
 """
