f19208e2f953ba4f59f4bf326930743b2de8da21
Tonya E <luis1944alex@gmail.com>
2023-05-03 13:15:16 -0400

    Added method for constructing dataframe from song lyrics.

--- server/server.py
+++ server/server.py
@@ -40,6 +40,40 @@ def get_Emotions_For_A_List_Of_Songs():
 
     return {'base64_encoded_gimage': b64encoded_string}, 200#{'response': map(lambda song: song.toJson(), classified) }), 200
 
+def construct_Data_Frame_from_Song(song):
+    df = pd.DataFrame({
+        "text": song.lyics,
+        "admiration": [0 for x in range(len(song.lyrics))],
+        "amusement": [0 for x in range(len(song.lyrics))],
+        "anger": [0 for x in range(len(song.lyrics))],
+        "annoyance": [0 for x in range(len(song.lyrics))],
+        "approval": [0 for x in range(len(song.lyrics))],
+        "caring": [0 for x in range(len(song.lyrics))],
+        "confusion": [0 for x in range(len(song.lyrics))],
+        "curiosity": [0 for x in range(len(song.lyrics))],
+        "desire": [0 for x in range(len(song.lyrics))],
+        "disappointment": [0 for x in range(len(song.lyrics))],
+        "disapproval": [0 for x in range(len(song.lyrics))],
+        "disgust": [0 for x in range(len(song.lyrics))],
+        "embarrassment": [0 for x in range(len(song.lyrics))],
+        "excitement": [0 for x in range(len(song.lyrics))],
+        "fear": [0 for x in range(len(song.lyrics))],
+        "gratitude": [0 for x in range(len(song.lyrics))],
+        "grief": [0 for x in range(len(song.lyrics))],
+        "joy": [0 for x in range(len(song.lyrics))],
+        "love": [0 for x in range(len(song.lyrics))],
+        "nervousness": [0 for x in range(len(song.lyrics))],
+        "optimism": [0 for x in range(len(song.lyrics))],
+        "pride": [0 for x in range(len(song.lyrics))],
+        "realization": [0 for x in range(len(song.lyrics))],
+        "relief": [0 for x in range(len(song.lyrics))],
+        "remorse": [0 for x in range(len(song.lyrics))],
+        "sadness": [0 for x in range(len(song.lyrics))],
+        "surprise": [0 for x in range(len(song.lyrics))],
+        "neutral": [0 for x in range(len(song.lyrics))],
+    })
+    return df # Return the dataframe of song-lyrics.
+
 if __name__ == "__main__":
    print("Spotify Emotions Server Started \nPress Ctrl+C to stop the server \nServing..")
    serve(app, host="10.0.0.231", port=8080)
