723006ec85b8b9b4fda37a4eb34a5b6c152fc225
Derek Costello <costellod@merrimack.edu>
2023-05-02 21:37:12 -0400

    Added training and evaluation. Testing begins...

--- server/imports.py
+++ server/imports.py
@@ -14,6 +14,7 @@ import pandas as pd
 # Torch
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 
 # JSON
