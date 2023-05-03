eaccca9730461bdc93ca00138dba5cabf55381f1
Derek Costello <costellod@merrimack.edu>
2023-05-03 14:12:20 -0400

    Updated IP for client-side. Updated server for more debugging

--- server/server.py
+++ server/server.py
@@ -5,10 +5,12 @@ import Graph_Code
 
 # Load the model on the 3 GoEmotions training sets.
 df = pd.concat(
-    map(pd.read_csv, ['data/goemotions_1.csv'""",
-                      'data/goemotions_2.csv',
-                      'data/goemotions_3.csv'"""]), ignore_index=True)
-model = Model(df)
+    map(pd.read_csv, ['data/goemotions_1.csv'#,
+                      #'data/goemotions_2.csv',
+                      #'data/goemotions_3.csv'
+                      ]),
+                      ignore_index=True)
+#model = Model(df)
 
 # Start the server
 app = Flask(__name__)
@@ -27,7 +29,11 @@ def get_Emotions_For_A_List_Of_Songs():
         #print(classified[i].lyrics)
 
     # Run Each song through the model
-    model.eval(classified[0].lyrics)
+        # Build dataframe from the song lyrics to pass to BERT model
+    song_df = construct_Data_Frame_from_Song(classified[0])
+    print(song_df)
+    return {'base64_encoded_gimage': 'you thought bruh'}, 200#{'response': map(lambda song: song.toJson(), classified) }), 200
+    #probs = model.eval()
 
     # Construct a DataFrame for emotions from the songs
     emotions = [song.emotions for song in classified]
@@ -41,37 +47,20 @@ def get_Emotions_For_A_List_Of_Songs():
     return {'base64_encoded_gimage': b64encoded_string}, 200#{'response': map(lambda song: song.toJson(), classified) }), 200
 
 def construct_Data_Frame_from_Song(song):
-    df = pd.DataFrame({
-        "text": song.lyics,
-        "admiration": [0 for x in range(len(song.lyrics))],
-        "amusement": [0 for x in range(len(song.lyrics))],
-        "anger": [0 for x in range(len(song.lyrics))],
-        "annoyance": [0 for x in range(len(song.lyrics))],
-        "approval": [0 for x in range(len(song.lyrics))],
-        "caring": [0 for x in range(len(song.lyrics))],
-        "confusion": [0 for x in range(len(song.lyrics))],
-        "curiosity": [0 for x in range(len(song.lyrics))],
-        "desire": [0 for x in range(len(song.lyrics))],
-        "disappointment": [0 for x in range(len(song.lyrics))],
-        "disapproval": [0 for x in range(len(song.lyrics))],
-        "disgust": [0 for x in range(len(song.lyrics))],
-        "embarrassment": [0 for x in range(len(song.lyrics))],
-        "excitement": [0 for x in range(len(song.lyrics))],
-        "fear": [0 for x in range(len(song.lyrics))],
-        "gratitude": [0 for x in range(len(song.lyrics))],
-        "grief": [0 for x in range(len(song.lyrics))],
-        "joy": [0 for x in range(len(song.lyrics))],
-        "love": [0 for x in range(len(song.lyrics))],
-        "nervousness": [0 for x in range(len(song.lyrics))],
-        "optimism": [0 for x in range(len(song.lyrics))],
-        "pride": [0 for x in range(len(song.lyrics))],
-        "realization": [0 for x in range(len(song.lyrics))],
-        "relief": [0 for x in range(len(song.lyrics))],
-        "remorse": [0 for x in range(len(song.lyrics))],
-        "sadness": [0 for x in range(len(song.lyrics))],
-        "surprise": [0 for x in range(len(song.lyrics))],
-        "neutral": [0 for x in range(len(song.lyrics))],
-    })
+    cols = ["text", "admiration", "amusement", "anger", "annoyance", "approval", "caring", 
+            "confusion", "curiosity", "desire", "disappointment", "disapproval", 
+            "disgust", "embarrassment", "excitement", "fear", "gratitude", "grief",
+            "joy", "love", "nervousness", "optimism", "pride", "realization", 
+            "relief", "remorse", "sadness", "surprise", "neutral"]
+
+    df = pd.DataFrame(columns=cols)
+
+    emotions = [x for x in cols if x != "text"]
+
+    df["text"] = song.lyrics
+
+    df[emotions] = 0
+
     return df # Return the dataframe of song-lyrics.
 
 if __name__ == "__main__":
