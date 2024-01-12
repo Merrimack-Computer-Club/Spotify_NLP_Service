2e689e1396208ab1f81d8ff47d4c9bb69875dd73
Alexander Elguezabal <luis1944alex@gmail.com>
2024-01-11 21:34:51 -0500

    Added firebase code.

--- server/setup.py
+++
@@ -1,7 +0,0 @@
-from imports import *
-from tensorflow.python.client import device_lib
-
-print(tf.__version__)
-print(device_lib.list_local_devices())
-print("\n\n\n")
-print(tf.config.list_physical_devices('GPU'))
